0f5a9fa7994996d3ab4b711631aaaaf161a51af9
Alexey Pryazhnikov <aecid.z@gmail.com>
2019-09-26 10:53:13 +0300

    Dockerfile fix

--- TCGUABot/Controllers/UpdateController.cs
+++ TCGUABot/Controllers/UpdateController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using TCGUABot.Data;
 using TCGUABot.Models.InlineQueryHandler;
 using Telegram.Bot.Types;
@@ -28,6 +29,8 @@ namespace TCGUABot.Controllers
 
         public async Task<OkResult> Webhook([FromBody]Update update)
         {
+            Console.WriteLine(JsonConvert.SerializeObject(update));
+
             var commands = Bot.Commands;
             var callbackHandlers = Bot.CallbackHandlers;
             var client = await Bot.Get();
